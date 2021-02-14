6573fbe4a9c496123f3e6fa5b8d5799269d4e84a
ertugrulcehlik <ertgrl_@hotmail.com>
2021-02-14 21:54:12 +0300

    KampIntro Projesi

---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+
+            Console.WriteLine(kategoriEtiketi);
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalis Butonu Goster");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artis Butonu Goster");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi Butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanici ayarlari butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+        }
+    }
+}
